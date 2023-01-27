2f2d1517c24d08156356545aa14467a9f028552c
Hugo Pavy <hugo.pavy@gmail.com>
2023-01-27 14:05:53 +0100

    pl

--- main.py
+++ main.py
@@ -5,7 +5,24 @@ import argparse
 import os 
 from pathlib import Path
 from os import system
-import re        
+import re
+
+
+        
+
+
+# pg.init()
+
+# def lecture_fichier(enter_file):
+#     '''lit le fichier et le met dans une liste '''
+#     map_liste = []
+#     with open(enter_file, 'r') as f : 
+#         for ligne in f :
+#             map_liste.append(list(ligne.strip()))
+#     return map_liste
+
+
+        
 
 pg.init()
 
@@ -28,8 +45,54 @@ def lecture_fichier(enter_file='map/map1.txt'):
 
 ################### fonctions draw ###########################
 
-perso={'x':2, 'y':2,'power':2,'life':10,'or':0,'potion':0,
-        'armes':0, 'shield':0}
+def draw_perso(pers):
+    rect = pg.Rect(perso['y'] * pixel + 1,
+    perso['x'] * pixel + 1,
+    pixel - 2, pixel - 2)
+    pg.draw.rect(screen, (255, 0, 255), rect)
+
+def draw_point(pos_x, pos_y):
+    rect = pg.Rect(pos_y * pixel + 1,
+    pos_x * pixel + 1,
+    pixel - 2, pixel - 2)
+    pg.draw.rect(screen, (199, 208, 0), rect)
+
+def draw_h(pos_x, pos_y):
+    rect = pg.Rect(pos_y * pixel + 1,
+    pos_x * pixel + 1,
+    pixel - 2, pixel - 2)
+    pg.draw.rect(screen, (179, 177, 145), rect)
+
+def draw_plus(pos_x, pos_y):
+    rect = pg.Rect(pos_y * pixel + 1,
+    pos_x * pixel + 1,
+    pixel - 2, pixel - 2)
+    pg.draw.rect(screen, (158, 253, 56), rect)
+
+def draw_alpha(pos_x, pos_y):
+    rect = pg.Rect(pos_y * pixel + 1,
+    pos_x * pixel + 1,
+    pixel - 2, pixel - 2)
+    pg.draw.rect(screen, (255, 0, 127), rect)
+
+
+def move_perso(deplacement, map_list, pers):
+    pos = (pers['x'], pers['y'])
+    new_pos = (pos[0] + deplacement[0], pos[1] + deplacement[1])
+    if map_list[new_pos[0]][new_pos[1]] in ['.', '+', '#']:
+        pers['x'], pers['y'] = new_pos[0], new_pos[1]
+        if map_list[pos[0]][pos[1]] == '.':
+            draw_point(*pos)
+            draw_perso(perso)
+        elif map_list[pos[0]][pos[1]] == '#':
+            draw_h(*pos)  
+            draw_perso(perso)   
+        elif map_list[pos[0]][pos[1]] == '+':
+            draw_plus(*pos)  
+            draw_perso(perso)    
+        elif map_list[pos[0]][pos[1]] == '@':
+            draw_alpha(*pos)
+            draw_perso(perso) 
 
         
 def detect_objects(pers , map):    
@@ -39,47 +102,41 @@ def detect_objects(pers , map):
         pers['or']+=1
     elif map[pers['x']][pers['y']] == '!':
         pers['armes']+=1
-    elif map[pers['x']][pers['y']] == 's':
-        pers['armures']+=1
 
 #chauffe-souris=='c'
 #monstre cache == 'K'
 #creeper == 'b'
-def monstre_autour(pers = perso, map = map_list):
-    dir=[(1,0),(-1,0),(0,1),(0,-1),
-        (1,1),(-1,-1),(-1,1),(1,-1)] 
-    clock = pg.time.Clock()
-        
-    for d in dir:
-        if map[pers['x']+d[0]][pers['y']+d[1]] == 'c':
-            c_life=3
-            
-            while c_life>0:
-                pers['life']+=-2
-                clock.tick(1)
-                if
-                    c_life += -pers['power']
-                
-            pers['life'] += 1
+def monstre_autour(pers , map):    
+    if map[pers['x']][pers['y']] == 'j':
+        pass
 
 
+def mort_perso(pers ):
+    if pers['life']<=0:
+        raise ValueError('you have been defeated')
+           
+
+def monstres_attaque(monstre, pers):
+    pass
 
 
 
 # initialisation de l'écran
 # longueur ecran
 
-larg_case = len(map_list[0])
-long_case = len(map_list)
-
+larg_case = 80
+long_case = 40
+width = 40 # largeur du rectangle en pixels
 pixel = 40 # hauteur du rectangle en pixels
 long_ecr = long_case*pixel
 larg_ecr = larg_case*pixel
 
+pg.init()
 screen = pg.display.set_mode((larg_ecr, long_ecr))
 running = True
 
-# On initialise la map
+
+################################### On initialise la map #########################################
 # Loop on all tiles
 map_list = lecture_fichier()
 
@@ -122,9 +179,6 @@ for i, row in enumerate(map_list) :
             pixel - 2, pixel - 2)
             pg.draw.rect(screen, (129, 20, 83), rect)
 
-if perso['life']<=0:
-    running = False
-    
 
 pg.display.update()
 
@@ -149,6 +203,6 @@ while running:
                 move_perso((0,-1), map_list, perso)
         
     pg.display.update()
-raise ValueError('you have been defeated')
+
 
 
